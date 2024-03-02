092091ed1e91978a588f58cfba948424c05bbcba
Tomasz Radliński <tomekr.konta@gmail.com>
2024-03-02 16:05:09 +0100

    added frontend directory

---
+++ frontend/service-worker.js
@@ -0,0 +1,47 @@
+// service-worker.js
+
+const CACHE_NAME = "my-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/apple-touch-icon.png",
+  "/favicon-32x32.png",
+  "/favicon-16x16.png",
+  "/fonts/digital-7.ttf",
+  "/fonts/digital-7 (italic).ttf",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      // Sprawdzenie, czy żądanie pochodzi z obsługiwanego schematu
+      if (!event.request.url.startsWith("http")) {
+        return fetch(event.request);
+      }
+
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
